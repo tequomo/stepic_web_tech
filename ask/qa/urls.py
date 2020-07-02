f3a5122d1e083a13d902e210a326a1c7d67ce832
Alexander <tequomo@gmail.com>
2020-07-03 01:37:43 +0300

    phase 3: wsgi

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from qa.views import index, popular, ask, login_view, signup
+from qa.views import test
 
 #urlpatterns = [
 #    url(r'^admin/', admin.site.urls),
