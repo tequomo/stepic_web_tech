4830c5ea1c13729758232ad01d3c246261210f25
Alexander <tequomo@gmail.com>
2020-07-03 01:39:16 +0300

    phase 3: wsgi

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
 #sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
 #sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
-sudo gunicorn ask.wsgi:application
+sudo gunicorn ask.wsgi.application
 #sudo /etc/init.d/mysql start
